fcd6858bc65411635cd556274fff6b553b859d55
Maria1564 <salyginamaria156@gmail.com>
2026-01-29 18:02:13 +0300

    add: подключена Prisma

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
-const AboutPage = () => {
+import prisma from "@/utils/prisma";
+
+const AboutPage = async () => {
+  const users = await prisma.user.findMany();
+
+  console.log("users >> ", users);
+
   return (
     <div>
       <h1>about page</h1>
